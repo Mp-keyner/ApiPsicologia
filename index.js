24e4dabf3c25be361e2cb0563a4f6e1d11dc2b5c
Mp-keyner <keyneroswaldo0@gmail.com>
2023-11-22 00:35:33 -0500

    Update Api

--- index.js
+++ index.js
@@ -10,11 +10,98 @@ app.listen(4000, () => {
   console.log("listening on port 4  000");
 });
 
-app.get("/", (req, res) => {
+
+    app.get("/", (req, res) => {
+        res.send([
+          {
+              id:1,
+              name: 'Dr. Oswaldo',
+              time: 12,
+              Likes: 18,
+              answers: 20,
+              theme: 'Nutricion',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:2,
+              name: 'Anonimo',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Nutricion',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:3,
+              name: 'Dr Pedro',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Nutricion',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:4,
+              name: 'Dr. Markus',
+              time: 12,
+              Likes: 18,
+              answers: 20,
+              theme: 'Salud',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:5,
+              name: 'Anonimo',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Salud',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:6,
+              name: 'Dr Pablo',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Salud',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:7,
+              name: 'Dr. Marce',
+              time: 12,
+              Likes: 18,
+              answers: 20,
+              theme: 'Medicinas',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:8,
+              name: 'Anonimo',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Medicinas',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          },
+          {
+              id:9,
+              name: 'Dr Angel',
+              time: 20,
+              Likes: 8,
+              answers: 2,
+              theme: 'Medicinas',
+              image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+          }
+        ]);
+       });
+
+app.get("/Nutricion", (req, res) => {
   res.send([
     {
         id:1,
-        name: 'Dr. Keyner',
+        name: 'Dr. Oswaldo',
         time: 12,
         Likes: 18,
         answers: 20,
@@ -27,7 +114,80 @@ app.get("/", (req, res) => {
         time: 20,
         Likes: 8,
         answers: 2,
+        theme: 'Nutricion',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    },
+    {
+        id:3,
+        name: 'Dr Pedro',
+        time: 20,
+        Likes: 8,
+        answers: 2,
+        theme: 'Nutricion',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    }
+  ]);
+});
+
+app.get("/Salud", (req, res) => {
+  res.send([
+    {
+        id:4,
+        name: 'Dr. Markus',
+        time: 12,
+        Likes: 18,
+        answers: 20,
         theme: 'Salud',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    },
+    {
+        id:5,
+        name: 'Anonimo',
+        time: 20,
+        Likes: 8,
+        answers: 2,
+        theme: 'Salud',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    },
+    {
+        id:6,
+        name: 'Dr Pablo',
+        time: 20,
+        Likes: 8,
+        answers: 2,
+        theme: 'Salud',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    }
+  ]);
+});
+
+app.get("/Medicinas", (req, res) => {
+  res.send([
+    {
+        id:7,
+        name: 'Dr. Marce',
+        time: 12,
+        Likes: 18,
+        answers: 20,
+        theme: 'Medicinas',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    },
+    {
+        id:8,
+        name: 'Anonimo',
+        time: 20,
+        Likes: 8,
+        answers: 2,
+        theme: 'Medicinas',
+        image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
+    },
+    {
+        id:9,
+        name: 'Dr Angel',
+        time: 20,
+        Likes: 8,
+        answers: 2,
+        theme: 'Medicinas',
         image: 'https://facelessapi.kiura.co/assets/avatars/Avatar-30.jpg'
     }
   ]);
