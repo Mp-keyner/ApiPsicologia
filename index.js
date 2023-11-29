63f16213b2e1031789a715273359413d7a20a1c4
mp-keyner <keyneroswaldo0@gmail.com>
2023-11-29 18:14:56 -0500

    29 de noviembre 6:14

--- index.js
+++ index.js
@@ -194,6 +194,94 @@ app.get("/Medicinas", (req, res) => {
 });
 
 
+app.get("/PopularDoctor", (req, res) => {
+  res.send([
+    {
+      name: "Dr. Oswaldo P.",
+      profession: "Psiquiatra",
+      hour: "18",
+      qualification: 9,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F4.png?alt=media&token=a9c6b891-281e-46dd-ab4d-0a9f91e9c521",
+    },
+    {
+      name: "Dr. Andres P.",
+      profession: "Psicóloga",
+      hour: "17",
+      qualification: 8,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F14.png?alt=media&token=d8ef4d27-5076-42d6-b43e-5678df9ff26e",
+    },
+    {
+      name: "Dr. Ana L.",
+      profession: "Psiquiatra",
+      hour: "12",
+      qualification: 9,
+      online: false,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F6.png?alt=media&token=444cc8b9-0035-4893-adf9-f273fd5be168",
+    },
+    {
+      name: "Dr. Pedro D.",
+      profession: "Terapeuta",
+      hour: "13",
+      qualification: 8,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F10.png?alt=media&token=c34171bb-a7d9-4c16-9a7c-557ce23921d4",
+    },
+    {
+      name: "Dra. Maria S.",
+      profession: "Psicóloga",
+      hour: "15",
+      qualification: 9,
+      online: false,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F1.png?alt=media&token=d90a9e3c-74c1-4aa1-94d4-22c9cda37bff",
+    },
+    {
+      name: "Dr. Carlos M.",
+      profession: "Psiquiatra",
+      hour: "14",
+      qualification: 7,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F11.png?alt=media&token=01553851-4d93-40d5-9168-3c31db3ac500",
+    },
+    {
+      name: "Dr. Luis G.",
+      profession: "Terapeuta",
+      hour: "16",
+      qualification: 8,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F12.png?alt=media&token=763ead81-6dca-4f16-ad5a-c3d5ab6915ac",
+    },
+    {
+      name: "Dr. Mario R.",
+      profession: "Terapeuta",
+      hour: "11",
+      qualification: 7,
+      online: false,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F15.png?alt=media&token=112dfe2e-af0a-47e1-9f88-f0e3be93aa84",
+    },
+    {
+      name: "Dr. Juan S.",
+      profession: "Terapeuta",
+      hour: "10",
+      qualification: 8,
+      online: true,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F3.png?alt=media&token=72f4a18e-1075-422f-bc35-47e9a0a17291",
+    },
+    {
+      name: "Dra. Sofia F.",
+      profession: "Psicóloga",
+      hour: "19",
+      qualification: 7,
+      online: false,
+      photo: "https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/doctor%2F5.png?alt=media&token=196f931a-5f2f-473e-967d-0466f80c0a8b",
+    }
+  ]
+  
+  );
+});
+
+
 // app.get("/usuarios", (req, res) => {
 //   res.send([]);
 // });
